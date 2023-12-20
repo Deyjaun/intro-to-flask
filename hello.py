d1c49fe690991d7f78dda2efe477395cc4d04145
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2023-12-20 10:50:47 -0500

    jinja

---
+++ hello.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index ():
+    return render_template("home.html.jinja")
+
+@app.route('/ping')
+def ping ():
+    return "<h1>PONG<h1>"
+@app.route('/hello/<name>')
+def hello(name):
+    return f"Hello {name}"
