9d8a81966bb15960740970930bdaa9a794db4245
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2024-01-02 10:50:52 -0500

    lists

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -8,6 +8,15 @@ todolist=['Do youtube','Get money']
 
 @app.route('/', methods= ['GET','POST'] )
 def index():
-    new_todo = request.form["new_todo"]
-    todolist.append(new_todo)
+    if request.method == 'POST':
+     new_todo = request.form["new_todo"]
+     todolist.append(new_todo)
+   
     return render_template("todo.html.jinja", my_todos=todolist)
+
+
+@app.route('/delete_todolist/<int:todo_index>', methods= ['POST'])
+def todo_delete(todo_index):
+   del todolist[todo_index]
+
+   return redirect('/')
