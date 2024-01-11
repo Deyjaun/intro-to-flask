c45ae53a78a2238194c92c637e3e168f2242174d
Deyjaun <Dlawrence@brooklynsteamcenter.org>
2024-01-11 10:51:11 -0500

    todo project

--- todo.py
+++ todo.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request, redirect
 import pymysql
 import pymysql.cursors
 from pprint import pprint as print
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
 app = Flask(__name__)
+
+auth = HTTPBasicAuth()
 
 conn = pymysql.connect(
   database = "dlawrence_todos",
@@ -15,12 +19,21 @@ conn = pymysql.connect(
   cursorclass=pymysql.cursors.DictCursor
 )
 
-
-
+users = {
+    "Deyjaun": generate_password_hash("Sally"),
+    "susan": generate_password_hash("bye")
+}
 
 todolist=['Do youtube','Get money']
 
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
 @app.route('/', methods= ['GET','POST'] )
+@auth.login_required
 def index():
     if request.method == 'POST':
      new_todo = request.form["new_todo"]
@@ -40,7 +53,8 @@ def index():
 
     print(results)
    
-    return render_template("todo.html.jinja", my_todos=results)
+    return render_template("todo.html.jinja", my_todos=results) 
+    return "Hello, {}!".format(auth.current_user())
 
 
 
